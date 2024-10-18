491e823c73404b2bbdf1fbc5d4a73152a7069d67
vinit.raut <vinit.coder@gmail.com>
2024-10-18 12:15:36 +0530

    add: jwt based authentication from db

--- src/services/apiAuth.ts
+++ src/services/apiAuth.ts
@@ -14,7 +14,7 @@ export async function loginApi(values: IPayload) {
     return data;
   } catch (error: unknown) {
     console.log(error);
-    return error;
+    throw new Error(error?.response?.data?.message ?? error);
   }
 }
 
